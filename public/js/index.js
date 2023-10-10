2cff5a0913e3f2956fd6643bb6a5d81ff077dac2
M4pacheDev <agustindiazmclh@gmail.com>
2023-10-10 20:05:41 -0300

    Fix(Proj): PlaceholderImage doesn't shows

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ console.log(images);
 if (images !== undefined) {
   for (let j = 0; j < images.length; j++) {
     images[j].addEventListener('error', () => {
-      images[j].src = '/img/placeHolder/placeholder-image.webp';
+      images[j].src = '/img/placeholder/placeholder-image.webp';
     });
   }
 }
