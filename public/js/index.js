bd9ce872ddebb5d5fb752733b22525314e7c6ddb
M4pacheDev <agustindiazmclh@gmail.com>
2023-10-10 20:13:20 -0300

    Fix(profile): Profile showed wrong placeholder

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,15 @@ let images = document.getElementsByTagName('img');
 console.log(images);
 
 if (images !== undefined) {
+  let src = '';
+  if (window.location.href.split('/')[4] === 'profile') {
+    src = 'avatar-placeholder.png';
+  } else {
+    src = '/img/placeholder/placeholder-image.webp';
+  }
   for (let j = 0; j < images.length; j++) {
     images[j].addEventListener('error', () => {
-      images[j].src = '/img/placeholder/placeholder-image.webp';
+      images[j].src = src;
     });
   }
 }
