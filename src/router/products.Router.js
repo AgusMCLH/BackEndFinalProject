ea90c1f9134098491c657266d7482860c4e1940c
pache_dev <agustindiazmclh@gmail.com>
2023-10-10 22:08:28 -0300

    FIX(Products): Bug

--- src/router/products.Router.js
+++ src/router/products.Router.js
@@ -30,9 +30,12 @@ export default class ProductsRouter extends CustomRouter {
         res.status(404).send(`No hay ningun producto con el ID: ${idBuscado}`);
       } else {
         const { product, hasImages, AddtoCartURL } = productObj;
+        logger.debug(`Product ${idBuscado} requested`);
         const user = req.session.user;
-        if (product.owner === user.email) {
-          product.visible = false;
+        if (req.session.user) {
+          if (product.owner === user.email) {
+            product.visible = false;
+          }
         }
         res.render('productPage', { product, hasImages, user, AddtoCartURL });
       }
