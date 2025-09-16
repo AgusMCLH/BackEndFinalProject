95c32dfb466879e7ad61028b33ee306375ba8959
Agus Diaz <agustindiazmclh@gmail.com>
2025-09-15 22:34:26 -0300

    Update products.DAO.js

--- src/DAO/Mongo/products.DAO.js
+++ src/DAO/Mongo/products.DAO.js
@@ -1,20 +1,20 @@
 import { productModel } from './../../models/product.model.js';
-import { io } from './../../app.js';
 import { logger } from './../../middleware/logger.middleware.js';
 
 class ProductDAO {
   constructor() {
     this.model = productModel;
   }
+
   async addProducts(product) {
     try {
       const data = await this.model.create(product);
-      io.emit('Products', await this.getProducts());
       return {
         code: 201,
-        msg: `el producto fue agregado correctamente con el id: ${data._id}`,
+        msg: `El producto fue agregado correctamente con el id: ${data._id}`,
       };
     } catch (error) {
+      logger.error(error);
       return {
         code: 500,
         msg: `Error de servidor - ${error}`,
@@ -37,6 +37,7 @@ class ProductDAO {
       }
       return data;
     } catch (error) {
+      logger.error(error);
       return {
         code: 500,
         msg: `Error de servidor - ${error}`,
@@ -49,6 +50,7 @@ class ProductDAO {
       const data = await this.model.find().lean();
       return data;
     } catch (error) {
+      logger.error(error);
       return {
         code: 500,
         msg: `Error de servidor - ${error}`,
@@ -59,7 +61,7 @@ class ProductDAO {
   async getProductByID(id) {
     try {
       const data = await this.model.findById(id).lean();
-      if (data == null) {
+      if (!data) {
         return {
           code: 400,
           msg: `No existe un producto con el id ${id}`,
@@ -67,6 +69,7 @@ class ProductDAO {
       }
       return data;
     } catch (error) {
+      logger.error(error);
       return {
         code: 500,
         msg: `Error de servidor - ${error}`,
@@ -77,43 +80,57 @@ class ProductDAO {
   async updateProduct(idAEditar, product) {
     try {
       const data = await this.model.findByIdAndUpdate(idAEditar, product);
-      if (data == null) {
+      if (!data) {
         return {
           code: 400,
           msg: `No existe un producto con el id ${idAEditar}`,
         };
       }
-      io.emit('Products', await this.getProducts());
       return {
         code: 200,
         msg: `El producto con el id ${idAEditar} fue actualizado correctamente`,
       };
     } catch (error) {
       logger.error(error);
-    }
-  }
-
-  async deleteProduct(id) {
-    try {
-      const data = await this.model.findByIdAndDelete(id);
-      io.emit('Products', await this.getProducts());
-      return {
-        code: 200,
-        msg: `El producto con el id ${id} fue elminado correctamente.`,
-      };
-    } catch (error) {
       return {
         code: 500,
         msg: `Error de servidor - ${error}`,
       };
     }
   }
+
+  async deleteProduct(id) {
+    try {
+      const data = await this.model.findByIdAndDelete(id);
+      if (!data) {
+        return {
+          code: 400,
+          msg: `No existe un producto con el id ${id}`,
+        };
+      }
+      return {
+        code: 200,
+        msg: `El producto con el id ${id} fue eliminado correctamente.`,
+      };
+    } catch (error) {
+      logger.error(error);
+      return {
+        code: 500,
+        msg: `Error de servidor - ${error}`,
+      };
+    }
+  }
+
   async getProductsByOwner(owner) {
     try {
-      const products = await this.model.find({ owner: owner }).lean();
+      const products = await this.model.find({ owner }).lean();
       return products;
     } catch (error) {
       logger.error(error);
+      return {
+        code: 500,
+        msg: `Error de servidor - ${error}`,
+      };
     }
   }
 }
